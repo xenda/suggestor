c2a01bcae1e1ba45b30e613a47557a7830f91451
Alvaro Pereyra <alvaro@xendacentral.com>
2011-09-23 23:25:39 -0500

    Simplifies engine quite a bit. Removes collection as a hash and uses plain initialization

--- lib/suggestor/engine.rb
+++ lib/suggestor/engine.rb
@@ -8,44 +8,29 @@ module Suggestor
 
     attr_accessor :collection, :algorithm_class, :algorithm
 
-    def initialize(algorithm=nil)
-      @collection = {}
-      algorithm ||= Suggestor::Algorithms::EuclideanDistance
-      @algorithm_class = algorithm
-    end
-
-    def load_data(input)
-      add_to_collection(input)
-      initialize_algorithm
-      collection
+    def initialize(input, algorithm=Algorithms::EuclideanDistance)
+      collection = parse_from_json(input)
+      @algorithm = algorithm.new(collection)
     end
     
     def similar_to(item, opts={})
-      algorithm.similar_to(item, opts)
+      @algorithm.similar_to(item, opts)
     end
 
     def recommended_to(item, opts={})
-      algorithm.recommended_to(item, opts)
+      @algorithm.recommended_to(item, opts)
     end
 
     def similar_related_to(item, opts={})
-      algorithm.similar_related_to(item, opts)
+      @algorithm.similar_related_to(item, opts)
     end
 
     private
     
-    def add_to_collection(input)
-      @collection.merge! parse_from_json(input)
-    end
-
     def parse_from_json(json)
       JSON.parse(json)
     rescue Exception => ex
       raise WrongInputFormat, "Wrong Data format: #{ex.message}" 
-    end
-
-    def initialize_algorithm
-      @algorithm = @algorithm_class.new(collection)
     end
 
   end
